e0c1afca5596c0d0e2af43b9dfc9f062f8a45785
Vanillamice <vanillamice@protonmail.com>
2020-12-14 18:31:38 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int* a, int* b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+/*pseduo code for the bubble sort function 
+we need to move for the size of the array and compare every 2 items together
+we start at arr[1] and compare it to arr[1-1] till we reach arr[n] and arr[n-1]
+
+*/
+
+void bubblesort(int arr[], int n)
+{
+    for (int breakcount = 0; breakcount < 5*5; breakcount++) {
+        for (int i = 1; i < n; i++) {
+            if (arr[i - 1] > arr[i]) {
+                swap(&arr[i], &arr[i - 1]);
+            }
+            else {
+                breakcount++;
+            }
+        }
+    }
+}
+void printArray(int arr[], int size)
+{
+    int m;
+    for (m = 0; m < size; m++)
+        printf("%d ", arr[m]);
+}
+
+int main()
+{
+    int arr[] = { 23,78,45,8,32,56 };
+    int n = 6;
+    bubblesort(arr, n);
+    printArray(arr, n);
+
+
+}
+
